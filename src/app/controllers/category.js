1b97035dd93cb80ad0ef7c55153d744f9988c4a4
Philipesissala <filipesissala14@gmail.com>
2022-02-27 16:30:09 +0100

    Finalizando categorias

--- src/app/controllers/category.js
+++ src/app/controllers/category.js
@@ -3,7 +3,7 @@ const database = require("../database/database");
 module.exports = {
   async index(req, res) {
     let response = await database.select().into("categorias")
-    res.render("admin/categories/index", { data: response });
+    res.render("admin/categories/index", { datas: response });
   },
   create(req, res) {
     res.render("admin/categories/new");
@@ -11,13 +11,33 @@ module.exports = {
 
   async save(req, res) {
     const designacao = req.body;
-    console.log(designacao)
     if (designacao != "") {
       let response = await database.insert(designacao).into("categorias");
       if (response > 0) {
-        console.log("Sucesso")
-        res.render("admin/categories/index");
+        res.redirect("/admin/categories");
       }
     }
   },
+  async delete(req, res) {
+    const id = req.params.id;
+    let response = await database.from("categorias").where("id", id).delete();
+    if (response > 0) {
+      res.redirect("/admin/categories");
+    }
+  },
+  async edit(req, res) {
+    let id = req.params.id;
+    let response = await database.select().into("categorias").where("id", id);
+    res.render("admin/categories/edit", { data: response[0] });
+  },
+  async update(req, res) {
+    let id = req.params.id;
+    let designacao = req.body.designacao;
+
+    let response = await database.update({ designacao: designacao }).where("id", id).table("categorias");
+    if (response > 0) {
+      res.redirect("/admin/categories");
+    }
+
+  }
 };
--- src/app/routes/category.js
+++ src/app/routes/category.js
@@ -6,5 +6,8 @@ const routes = express.Router();
 routes.get("/admin/categories", category.index);
 routes.get("/admin/categories/new", category.create);
 routes.post("/admin/categories/save", category.save);
+routes.post("/admin/category/delete/:id", category.delete);
+routes.get("/admin/category/edit/:id", category.edit);
+routes.post("/admin/category/update/:id", category.update);
 
 module.exports = routes;
